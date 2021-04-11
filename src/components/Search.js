77889dca45b6b14898e42d5d573c5383d15f4b87
jtreeves <jackson.t.reeves@gmail.com>
2021-04-10 22:15:01 -0400

    [search] Build functionality to access book from backend

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,8 +1,44 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+
+const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL
+
 function Search() {
+    const [results, setResults] = useState([])
+
+    const author = "Plato"
+
+    async function getResults() {
+        try {
+            const collection = await axios.get(
+                REACT_APP_SERVER_URL + '/books/' + author
+            )
+            const collectionArray = collection.data.books.map((book, index) => {
+                return (
+                    <div 
+                        key={index}
+                    >
+                        <h3>{book.title}</h3>
+                        <h4>by {book.author}</h4>
+                        <p>Subjects: {book.subjects}</p>
+                    </div>
+                )
+            })
+            setResults(collectionArray)
+        } catch (error) {
+            alert('FAILED')
+        }
+    }
+    
+    useEffect(() => {
+        getResults()
+    }, [])
+
     return (
         <div>
             <h2>Search</h2>
             <p>Find a book!</p>
+            {results}
         </div>
     )
 }
